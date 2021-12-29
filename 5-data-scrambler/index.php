1b6bf495567998211ad4ed86c07de14e4e5ae10f
Sazzad Mahmud Hredoy <mahmudsazzad85@gmail.com>
2021-12-29 11:14:23 +0600

    commit

---
+++ 5-data-scrambler/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="//cdn.rawgit.com/necolas/normalize.css/master/normalize.css">
+    <link rel="stylesheet" href="//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css">
+    <style>
+        body {
+            margin-top: 30px;
+        }
+    </style>
+    <title>Data Scrambler</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="column column-60 column-offset-20">
+                <h1>Welcome to Data Scrambling</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero autem nesciunt quaerat nihil praesentium, vitae facere similique doloribus enim voluptatum, consectetur eos ex delectus, aut quibusdam a quia? Ut qui quos, temporibus nihil deleniti doloribus voluptatibus reiciendis ipsa aspernatur magnam repellendus necessitatibus vero, accusamus assumenda architecto! Voluptate sit libero harum?</p>
+                <button>Read More</button>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
